e662cae9057c8e4533737b7f48303103d5a85640
Roman Broshnitskiy <romario777v8@gmail.com>
2021-12-29 00:17:18 +0200

    add statistics component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import user from "./json/user.json";
 import data from "./json/data.json";
 import friends from "./json/friends.json";
+import transactions from "./json/transactions.json";
 import { Profile } from "./components/Profile/Profile";
 import { Statistics } from "./components/Statistics/Statistics";
 import { FriendList } from "./components/FriendList/FriendList";
+import { TransactionHistory } from "./components/TransactionHistory/TransactionHistory";
 
 export default function App() {
   return (
@@ -13,6 +15,8 @@ export default function App() {
       <Statistics title="Upload stats" stats={data} />
 
       <FriendList friends={friends} />
+
+      <TransactionHistory items={transactions} />
     </>
   );
 }
