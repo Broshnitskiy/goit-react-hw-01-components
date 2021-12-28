6a43a5acb8f6f4355b533eebe14dcec4d8096faa
Roman Broshnitskiy <romario777v8@gmail.com>
2021-12-28 22:40:39 +0200

    добавляє компоненти

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from "./logo.svg";
-import "./App.css";
+import user from "./json/user.json";
+import data from "./json/data.json";
+import { Profile } from "./components/Profile/Profile";
+import { Statistics } from "./components/Statistics/Statistics";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Profile user={user} />
+
+      <Statistics title="Upload stats" stats={data} />
+    </>
   );
 }
 
-export default App;
+// import logo from "./logo.svg";
+// import "./App.css";
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
